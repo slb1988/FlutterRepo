157b952a5eca24018d40dd1bdc2c72d4cf0eddf0
luckey <sunlaibing88@gmail.com>
2019-03-28 19:49:55 +0800

    A mix-and-match approach

--- flutter_managestate/lib/main.dart
+++ flutter_managestate/lib/main.dart
@@ -58,10 +58,74 @@ class _ParentWidgetState extends State<ParentWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TapboxB(
+      child: TapboxC(
          active: _active,
         onChanged: _handleTapboxChanged,
       ),
+    );
+  }
+}
+
+class TapboxC extends StatefulWidget {
+  TapboxC({Key key, this.active: false, @required this.onChanged})
+    : super (key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  _TapboxCState createState() => _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handeTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _handleTapDown,
+        onTapUp: _handleTapUp,
+        onTapCancel: _handleTapCancel,
+        onTap: _handeTap,
+        child: Container(
+          child: Center(
+            child: Text(widget.active ? "Active" : 'Inactive',
+              style: TextStyle(fontSize: 32.0, color: Colors.white),),
+          ),
+          width: 200.0,
+          height: 200.0,
+            decoration: BoxDecoration(
+              color:
+                widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+              border: _highlight ?
+                  Border.all(
+                    color: Colors.teal[700],
+                    width: 10.0,
+                  )
+                  : null,
+            ),
+        ),
     );
   }
 }
