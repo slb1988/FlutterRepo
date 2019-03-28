f42b0bd433da6a88ebde017c75669d1532d3221f
luckey <sunlaibing88@gmail.com>
2019-03-28 19:38:40 +0800

    The parent widget manages the widget’s state

--- flutter_managestate/lib/main.dart
+++ flutter_managestate/lib/main.dart
@@ -37,6 +37,67 @@ class _TapboxAState extends State<TapboxA> {
   }
 }
 
+// ParentWidget manages the state for TapboxB.
+
+//------------------------ ParentWidget --------------------------------
+
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapboxB(
+         active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+class TapboxB extends StatelessWidget {
+  TapboxB({Key key, this.active: false, @required this.onChanged})
+    : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+        child: Container(
+          child: Center(
+            child: Text(
+              active ? 'Active' : 'Inactive',
+              style: TextStyle(fontSize: 32.0, color: Colors.white),
+            ),
+          ),
+          width: 200.0,
+            height: 200.0,
+            decoration: BoxDecoration(
+              color: active ? Colors.lightGreen[700] : Colors.grey[600],
+            ),
+        ),
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -48,7 +109,7 @@ class MyApp extends StatelessWidget {
           title: Text('Flutter Demo'),
         ),
         body: Center(
-          child: TapboxA(),
+          child: ParentWidget(),
         ),
       )
     );
