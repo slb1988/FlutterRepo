09a39fb8bfb24cdbc2b0e5402da87507db2f14df
luckey <sunlaibing88@gmail.com>
2019-03-28 18:25:59 +0800

    Step 5: Implement the image section

--- example_layout/lib/main.dart
+++ example_layout/lib/main.dart
@@ -90,6 +90,11 @@ class MyApp extends StatelessWidget {
           ),
           body: Column(
             children: <Widget>[
+              Image.asset('images/lake.jpg',
+                width: 600,
+                  height: 240,
+                  fit: BoxFit.cover,
+              ),
               titleSection,
               buttonSection,
               textSection,
