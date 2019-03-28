d19a2509d1992c215e64e6e6615c08b3a0c9d793
luckey <sunlaibing88@gmail.com>
2019-03-28 18:40:47 +0800

    Step 3: Subclass State

--- example_layout/lib/main.dart
+++ example_layout/lib/main.dart
@@ -134,9 +134,37 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
   bool _isFavorited = true;
   int _favoriteCount = 41;
 
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(0),
+            child: IconButton(icon: _isFavorited ? Icon(Icons.star) : Icon(Icons.star_border),
+                color: Colors.red[500],
+                onPressed: _toggleFavorite,
+            ),
+          ),
+          SizedBox(
+            width: 18,
+              child: Container(
+                child: Text('$_favoriteCount'),
+              ),
+          )
+        ],
+    );
   }
 }
